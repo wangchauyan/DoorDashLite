5c408b02ebc3f7eb4fa2ee3b3202ae91e00a6100
chauyan <wang.chauyan@gmail.com>
2020-01-20 00:10:49 -0800

    refactor: Add more test behaviors for the UI test of the RestaurantListFragment
    Add assertion for testing the restaurant list view and item counts in the adapter.
    
    - Chauyan

--- app/src/androidTest/java/idv/chauyan/doordashlite/presentation/screen/restaurant_list/RestaurantListFragmentTest.kt
+++ app/src/androidTest/java/idv/chauyan/doordashlite/presentation/screen/restaurant_list/RestaurantListFragmentTest.kt
@@ -1,12 +1,14 @@
 package idv.chauyan.doordashlite.presentation.screen.restaurant_list
 
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import idv.chauyan.doordashlite.R
 import idv.chauyan.doordashlite.mockserver.TestWebServer
 import idv.chauyan.doordashlite.presentation.screen.restaurant_list.view.MockRestaurantListFragment
@@ -35,7 +37,17 @@ class RestaurantListFragmentTest {
   @Test
   fun shouldDisplayRetaurantList() {
 
-    launchFragmentInContainer<MockRestaurantListFragment>()
+    val scenario = launchFragmentInContainer<MockRestaurantListFragment>()
+    scenario.onFragment {
+      val restaurantListView = it.activity?.findViewById<RecyclerView>(R.id.list)
+      val itemCount = restaurantListView?.adapter?.itemCount
+
+      assertThat(restaurantListView).isNotNull()
+      assertThat(itemCount).isNotNull()
+      assertThat(itemCount).isEqualTo(1)
+    }
     onView(withId(R.id.list)).check(matches(isDisplayed()))
+
+
   }
 }
